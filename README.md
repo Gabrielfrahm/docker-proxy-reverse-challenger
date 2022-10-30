f6cb8241158906cbd04dd7a9be20a98554d6359b
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-10-30 15:04:55 -0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Desafio proxy reverso nginx
+
+### Descrição :
+
+Voce ira perceber que dentro da pasta ./nginx temos o arquivo `default.conf` no proxy_pass tive que utilizar meu ip da vm da WSL2, porem irei subir com o localhost setado.
+
+A porta padrão que estou exportando do nodejs é 3333
---
+++ node/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const port = 3333;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+const dropTable = `DROP TABLE people`;
+const createTable = `CREATE TABLE  people (id int not null auto_increment, name varchar(20) not null, primary key (id))`;
+const sql = `INSERT INTO people(name) VALUES('Gabriel')`;
+const sql2 = `INSERT INTO people(name) VALUES('Wesley')`;
+const selectPeopleQuery = `SELECT * FROM people`;
+
+connection.connect();
+connection.query(dropTable);
+connection.query(createTable);
+connection.query(sql);
+connection.query(sql2);
+
+app.get("/", (req, res) => {
+  try {
+    let resultPeople = [];
+    connection.query(selectPeopleQuery, function (err, result, fields) {
+      if (err) throw err;
+      Object.keys(result).forEach(function (key) {
+        var row = result[key];
+        resultPeople.push(row.name);
+      });
+
+      res.send(`
+        <body>
+            <h1>Full Cycle Rocks!</h1>
+            <h3>Lista de usuários</h3>
+            <p>- ${resultPeople}</p>
+        </body>
+    `);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// connection.end();
+
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
